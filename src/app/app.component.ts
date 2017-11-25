1f29dbf93005d5b3b9e3345053a81e821151aec3
atreus <joakim.heldmann@gmail.com>
2017-11-25 17:08:43 +0100

    asd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 
-import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
+//import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
+import { TabsPage } from '../pages/tabs/tabs';
 
 
 
@@ -14,7 +15,7 @@ import { TabsControllerPage } from '../pages/tabs-controller/tabs-controller';
 })
 export class MyApp {
   
-  rootPage:any = TabsControllerPage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
