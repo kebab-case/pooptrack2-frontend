7c1e578f353181f7e5d416f64e506e2be98f2164
atreus <joakim.heldmann@gmail.com>
2017-11-25 20:04:45 +0100

    multiple markers

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -10,12 +10,22 @@ export class MapPage {
   title: string = 'My first AGM project';
   lat: number = 57.695143;
   lng: number = 11.981348;
-  zoomControl: boolean = false;
+  zoomControl: boolean = true;
   streetViewControl: boolean = false;
   disDoubleClickZoom: boolean = true;
   zoom: number;
   flat: boolean = true;
   markerClickable: boolean = true;
+  public markers = [
+    {
+      lat: 57.695143,
+      lng: 11.981348
+    },
+    {
+      lat: 57.595143,
+      lng: 11.971348
+    }
+  ];
 
   // this tells the tabs component which Pages
   // should be each tab's root Page
